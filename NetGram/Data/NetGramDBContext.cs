0d0dc19cf0dc07cf4975aaecf30e9192d1bb3785
juliely8619 <july8619@yahoo.com>
2019-02-13 12:00:55 -0800

    seeded data

--- NetGram/Data/NetGramDBContext.cs
+++ NetGram/Data/NetGramDBContext.cs
@@ -13,6 +13,47 @@ namespace NetGram.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(
+                new Post
+                {
+                    ID = 1,
+                    Author = "Bob Smith",
+                    Description = "a really cool description",
+                    ImageURL = "tempImage.jpg"
+                },
+                new Post
+                {
+                    ID = 2,
+                    Author = "Sally Smith",
+                    Description = "a not cool description",
+                    ImageURL = "tempImage2.jpg"
+                },
+                new Post
+                {
+                    ID = 3,
+                    Author = "John Smith",
+                    Description = "a lame description",
+                    ImageURL = "tempImage3.jpg"
+                }, 
+                new Post
+                {
+                    ID = 4,
+                    Author = "Jane Smith",
+                    Description = "just a description",
+                    ImageURL = "tempImage4.jpg"
+                },
+                new Post
+                {
+                    ID = 5,
+                    Author = "Billy Smith",
+                    Description = "a secret description that if I tell you I have to k*** you ;)",
+                    ImageURL = "tempImage5.jpg"
+                }
+                );
+        }
+
         public DbSet<Post> PostsTable { get; set; }
     }
 }
