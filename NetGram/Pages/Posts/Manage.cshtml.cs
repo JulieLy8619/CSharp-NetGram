e5de49a29dcd6ba736a18b67d614a7e605a7e9b4
juliely8619 <july8619@yahoo.com>
2019-02-14 16:47:38 -0800

    linked blob and imageURL tog

--- NetGram/Pages/Posts/Manage.cshtml.cs
+++ NetGram/Pages/Posts/Manage.cshtml.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage.Blob;
 using NetGram.Models;
 using NetGram.Models.Interfaces;
+using NetGram.Models.Utility;
 
 namespace NetGram.Pages.Posts
 {
@@ -13,15 +18,21 @@ namespace NetGram.Pages.Posts
     {
         private readonly INetGram _netgram;
 
-        public ManageModel(INetGram netgram)
+        public ManageModel(INetGram netgram, IConfiguration configuaton)
         {
             _netgram = netgram;
+            BlobImg = new Blob();
         }
         [FromRoute]
         public int? ID { get; set; }
 
         [BindProperty]
         public Post Post { get; set; }
+
+        [BindProperty]
+        public IFormFile Image { get; set; }
+
+        public Blob BlobImg { get; set; }
 
         public async Task OnGet()
         {
@@ -35,6 +46,22 @@ namespace NetGram.Pages.Posts
             tempPost.Author = Post.Author;
             tempPost.Description = Post.Description;
             tempPost.ImageURL = Post.ImageURL;
+
+            if (Image != null)
+            {
+                var filePath = Path.GetTempFileName();
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await Image.CopyToAsync(stream);
+                }
+
+                var container = await BlobImg.GetContainer("");//need my container name I create on azure to go in the quotes
+
+                BlobImg.AddBlob(Image.FileName, container, filePath);
+
+                CloudBlob returnBlob = await BlobImg.GetBlob(Image.FileName, container.Name);
+                tempPost.ImageURL = returnBlob.Uri.ToString();
+            }
 
             await _netgram.Save(tempPost);
             return RedirectToPage("/Posts/Index", new { id = tempPost.ID});
